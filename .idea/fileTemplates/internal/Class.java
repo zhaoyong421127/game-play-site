3ceb7660d8cd21eb8887d8a578a1ab8343595475
zhao.yong <zhao.yong@Ctrip.com>
2017-12-29 19:18:49 +0800

    commit

---
+++ .idea/fileTemplates/internal/Class.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* author:${USER}
+* createTime:${YEAR}-${MONTH}-${DATE}  ${TIME}
+*/
+public class ${NAME} {
+}
---
+++ src/main/java/com/xm/play/AppBootstrap.java
@@ -0,0 +1,17 @@
+package com.xm.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * zhao.yong
+ *
+ */
+@SpringBootApplication
+public class AppBootstrap
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AppBootstrap.class,args);
+    }
+}
